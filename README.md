c152b711bb0937912e2f96567709c618c3b914b7
Tejas-pr <Tejas.teju02@gmail.com>
2025-02-22 20:16:09 +0530

    docker init

---
+++ README.md
@@ -0,0 +1,23 @@
+# DEVOPS
+
+# learning how to setup the docker, docker-compose.
+
+## Manual installation
+Install nodejs locally ()
+Clone the repo
+Install dependencies (npm install)
+Start the DB locally
+docker run -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:5432 postgres
+Go to neon.tech and get yourself a new DB
+Change the .env file and update your DB credentials
+npx prisma migrate
+npx prisma generate
+npm run build
+npm run start
+
+## doceker installation
+
+
+
+
+## docker compose installtion setup
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+const prismaClient = new PrismaClient();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    const user = await prismaClient.user.findMany();
+    res.json({
+        user,
+        message: "Get request"
+    });
+})
+
+app.post("/", async (req, res) => {
+    await prismaClient.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    });
+    res.json({
+        message: "Post request"
+    });
+})
+
+app.listen(3000,() => {
+    console.log("listing on 3000");
+})
